5ed1c2aef2b84ee0a1a5978fcdc7a1e269930238
Leonardo Coltri <leocoltri1.1@gmail.com>
2021-08-08 17:17:33 -0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Login from './features/login/login';
+
+import {Router, Switch, Route } from 'react-router-dom';
+import routerHistory from './core/router/routerHistory';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={routerHistory}>
+    <Switch>
+      <Route path="/" exact={true} component={App}/>
+      <Route path="/login" exact={true} component={Login}/>
+    </Switch>
+  </Router>,
+
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
